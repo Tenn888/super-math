18323987706827e1ef3b09e55862b20230c8481a
Tenn888 <vladislavvasilev2009@gmail.com>
2025-03-02 17:12:16 +0300

    Add factorial rec func

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
 import modules.perimeter as perimeter
+import modules.factorial as factorial
+
+f1 = factorial.factorial_rec(5)
+print(f1)
+
+f2 = factorial.factorial_rec(15)
+print(f2)
 
 p1 = perimeter.perimeter_func([1, 2, 5])
 print(p1) 
 
-p1 = perimeter.perimeter_func([1, 2, 5, 7, 9])
-print(p1) 
+p2 = perimeter.perimeter_func([1, 2, 5, 7, 9])
+print(p2) 
