92f9c83825862bcf324de64d1aa2d74996d0fe0e
Tenn888 <vladislavvasilev2009@gmail.com>
2025-03-02 17:23:18 +0300

    Edit

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import modules.perimeter as perimeter
 import modules.factorial as factorial
 
-f1 = factorial.factorial_func(5)
+f1 = factorial.factorial_norm(5)
 print(f1)   
 
-f2 = factorial.factorial_func(15)
+f2 = factorial.factorial_norm(15)
 print(f2)
 
 f3 = factorial.factorial_rec(5)
